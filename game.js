08a3e0ae1e6706d6546a821ce87ccda4f2388141
Kim Coterwas <Kimberly.Coterwas1@marist.edu>
2014-11-13 17:00:30 -0500

    Sorta Finished Lab 10

--- game.js
+++ game.js
@@ -1,33 +1,57 @@
-var runSomeTests = function(){
-    var actionLabel = document.getElementById('action');
+var Player = {
+    items: [],
+    pickup: function(item){
+            return this.items.push(item);
+    },
+    drop: function(item){
+          if(this.items.indexOf(item) > 0) {
+             items.splice(indexOf(item), 1)
+          }else{
+             return false;
+          }
+     }
+}
 
-    console.log(actionLabel.tagName);
-    console.log(descrip.tagName);
-    console.log(descrip.innerHTML);
-    console.log(descrip.parentNode.tagName);
 
-    var headings = document.querySelectorAll('aside > h1');
- 
-    for (i = 0; i < headings.length; i++){
-        console.log(headings[i].innerHTML);
+
+var interpret = function(str){
+    console.log('calling interpret');
+    var Obj = {}
+    //error "cannot read property 'trim' of undefined"
+    str = str.trim();
+    var words = str.split(" ");
+    Obj.action = words.shift();
+    Obj.object = words.join();
+    return Obj;
+}
+
+var execute = function(command){
+    console.log('calling execute');
+    Player[command.action](command.object);
+}
+
+var report = function(){
+    console.log('calling report');
+    for(i in Player.items){
+        return document.querySelector("#inventory > ul");
     }
+}
 
-    console.log(inventory.tagName);
-    console.log(inventory.innerHTML);
-    console.log(inventory.parentNode.tagName);
+var gameStep = function(){
+    var input = this.value;
+    var object = interpret(input);
+    execute(object);
+    report();
+}
 
-    var inputText = document.getElementById('action');
-    console.log(inputText.placeholder);
-    
-    console.log(document.querySelector('body'));
-    //following doesn't do anything
-    var lookAtMyBody = document.querySelectorAll('body > h1');
-    for (i = 0; i < lookAtMyBody.length; i++){
-        console.log(lookAtMyBody[i].innerHTML);
-    }
+var gameStart = function(){
+    var inputBox = document.querySelector("input");
+    inputBox.addEventListener("keyup", function(event){
+            if(event.keyCode === 13){
+                    gameStep();
+            }
+    })
+}
 
+window.onload = gameStart;
 
-};
-
-
-window.onload = runSomeTests;
